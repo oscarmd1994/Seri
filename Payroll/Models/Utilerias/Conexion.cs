70b19f26384775a7cb8c1897af0f136bb2828cad
OscarMD <oscarmd1994@gmail.com>
2020-02-11 14:35:48 -0600

    cambio empresas

--- Payroll/Models/Utilerias/Conexion.cs
+++ Payroll/Models/Utilerias/Conexion.cs
@@ -9,10 +9,10 @@ namespace Payroll.Models.Utilerias
     public class Conexion
     {
 
-        //static readonly string Server = "OSCARMD";
-        //static readonly string Db = "db_payroll";
-        //static readonly string User = "sa";
-        //static readonly string Pass = "OSCAR";
+        //static readonly string Server = "GSERIPROD01";
+        //static readonly string Db = "IPSNet";
+        //static readonly string User = "IPSNet";
+        //static readonly string Pass = "IPSNet2";
         //*
         //static readonly string Server = "192.168.51.9";
         //static readonly string Db = "IPSNet";
